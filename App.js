c005d726ec98e506cad16d83988a229d00a5ffee
kobithan <87357540+kobithan@users.noreply.github.com>
2021-12-07 11:56:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -15,26 +15,51 @@ import {
     
 } from 'react-native';
 import { useDimensions,useDeviceOrientation} from '@react-native-community/hooks';
-import {createStackNavigator,createAppContainer} from 'react-navigation';
+
 import WelcomeScreen from './app/assets/screens/WelcomeScreen';
 import LoginScreen from './app/assets/screens/LoginScreen';
 import SignInScreen from './app/assets/screens/SignInScreen';
 import RootStackScreen from './app/assets/screens/RootStackScreen';
 import Test from './app/assets/screens/Test';
 import Test2 from './app/assets/screens/Test2';
-import HomeScreen from './app/assets/screens/HomeScreen';  
-import ProfileScreen from './app/assets/screens/ProfileScreen';  
 
 
 
 
-const AppNavigator = createStackNavigator(  
-  {  
-      Home: HomeScreen,  
-      Profile: ProfileScreen  
-  },  
-  {  
-      initialRouteName: "Home"  
-  }  
-);  
-export default createAppContainer(app);  
+export default function App() {
+  console.log("App executed")
+  //to see the virtual screen's height, scale,width
+  //console.log(useDimensions(), useDeviceOrientation());
+
+  const {landscape} = useDeviceOrientation();
+
+  const handlePress = () =>console.log("Text pressed");
+
+ 
+  return (
+    
+    //<WelcomeScreen/>
+  //<LoginScreen/>
+    <SignInScreen/>
+    //<Test/>
+    //<Test2/>
+    
+  );
+}
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    //justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 40 : 0,
+    
+  },
+});
